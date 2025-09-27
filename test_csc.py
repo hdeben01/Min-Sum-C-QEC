4665739fc2436f4463199abfa44cdcf0ed71c345
hugo <hdeben01@ucm.es>
2025-09-27 02:37:58 +0200

    output from csc implementation and time measure

--- test_csc.py
+++ test_csc.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     show_prints = False
     
     # List of codes to test in this example is the [[72, 12 ,6]] from https://www.nature.com/articles/s41586-024-07107-7
-    codesConfig = ["72"]
+    codesConfig = ["144"]
     
     # Number of Monte Carlo trials for physical error rates
     exp = 5
@@ -116,7 +116,7 @@ if __name__ == "__main__":
         pcm = sparse.csc_matrix(code.hx, dtype=np.uint8)    
                 
         # Code distance
-        d = 6
+        d = 12
         num_iterations = 100
      
 
@@ -226,7 +226,7 @@ if __name__ == "__main__":
                 a = time.time()
                 #predicted_errors_osd = _bposd.decode(detectors[0])
                 L_array = compute_min_sum_wrapper(sm, detectors[0].astype(np.int32), pcm.shape[0], pcm.shape[1],
-                                                    Lj.astype(np.double), alpha, num_iterations, error_computed)
+                                                    Lj.astype(np.double), alpha, num_iterations + 1, error_computed)
                 
                
                 
